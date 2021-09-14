62c2fa8a7d3620411f0428ed3cfa95d76c9ee226
vmundha1199@gmail.com <vmundha1199@gmail.com>
2021-09-14 16:58:14 +0530

    day 5 complete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,7 +6,7 @@ import 'package:flutter/services.dart';
 import 'dart:convert';
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -18,38 +18,76 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     loadData();
   }
-  Future<void> loadData() async{
-  final catalogJson =
-        await rootBundle.loadString("assets/files/catalog.json");
-        final dynamic decodedData = jsonDecode(catalogJson);
-        final List<dynamic> productsData= decodedData['products'] as List <dynamic>;
-        CatalogModel.items = List<Map>.from(productsData)
-        .map<Item>((item) => Item.fromMap(item as Map<String, dynamic>))
+
+  Future<void> loadData() async {
+    await Future<dynamic>.delayed(Duration(seconds: 2));
+    final catalogJson =
+        await rootBundle.loadString('assets/files/catelog.json');
+    final dynamic decodedData = jsonDecode(catalogJson);
+    final List<dynamic> productsData = decodedData['products'] as List<dynamic>;
+    CatalogModel.items = List<dynamic>.from(productsData)
+        .map<Item>((dynamic item) => Item.fromMap(item as Map<String, dynamic>))
         .toList();
-        setState(() {
-          
-        });
+    setState(() {});
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Catelog App'
-        ),
+        title: const Text('Catelog App'),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 2),
-        child: ListView.builder(
-          itemCount: CatalogModel.items.length,
-          itemBuilder: (BuildContext context, int index){
-            return ItemWidget(
-              item: CatalogModel.items[index]);
-          }
-        ),
+        padding: const EdgeInsets.all(16.0),
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16),
+                itemBuilder: (BuildContext context, int index) {
+                  final Item item = CatalogModel.items[index];
+                  return Card(
+                       clipBehavior: Clip.antiAlias,
+                       shape: RoundedRectangleBorder(
+                       borderRadius: BorderRadius.circular(15)),
+                      child: GridTile(
+                        header: Container(
+                          child: Text(
+                            item.name,
+                            style: const TextStyle(color: Colors.white),
+                            ),
+                            decoration: const BoxDecoration(
+                          color: Colors.deepPurple,
+                        ),
+                        padding: const EdgeInsets.all(12),
+                        ),
+
+                        child: 
+                         Image.network(
+                           item.image
+                           ),
+
+                        footer: Container(
+                          child: Text(
+                            item.price.toString(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.black,),
+                            ),
+                            padding: const EdgeInsets.all(12),
+                            decoration: const BoxDecoration(
+                              color: Colors.black54
+                            ),
+                            ),
+                        ));
+                },
+                itemCount: CatalogModel.items.length,
+              )
+            : const Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: const MyDrawer(),
     );
-
   }
 }
