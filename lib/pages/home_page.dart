eb36069ce3ec65bc8a9a501428298278b52df0a2
vmundha1199@gmail.com <vmundha1199@gmail.com>
2021-09-14 12:58:55 +0530

    dat 5 complete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,35 @@
+import 'package:catelog/models/catelog.dart';
 import 'package:catelog/widgets/drawer.dart';
+import 'package:catelog/widgets/items_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({ Key? key }) : super(key: key);
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+  Future<void> loadData() async{
+  final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+        final dynamic decodedData = jsonDecode(catalogJson);
+        final List<dynamic> productsData= decodedData['products'] as List <dynamic>;
+        CatalogModel.items = List<Map>.from(productsData)
+        .map<Item>((item) => Item.fromMap(item as Map<String, dynamic>))
+        .toList();
+        setState(() {
+          
+        });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,12 +38,17 @@ class HomePage extends StatelessWidget {
           'Catelog App'
         ),
       ),
-      body: Center(
-        child: Container(
-          child: Text('Welcome Vinay'),
-          ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 2),
+        child: ListView.builder(
+          itemCount: CatalogModel.items.length,
+          itemBuilder: (BuildContext context, int index){
+            return ItemWidget(
+              item: CatalogModel.items[index]);
+          }
+        ),
       ),
-      drawer: MyDrawer(),
+      drawer: const MyDrawer(),
     );
 
   }
---
+++ lib/widgets/items_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:catelog/models/catelog.dart';
+
+class ItemWidget  extends StatelessWidget {
+  final Item item;
+  
+
+  // ignore: sort_constructors_first
+  const ItemWidget ({Key? key, required this.item})
+    : assert(item != null),
+    super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0.0,
+      child: ListTile(
+        onTap: (){
+          print('${item.price} pressed');
+        },
+        leading: Image.network(item.image),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text(
+          '\$${item.price}',
+          textScaleFactor: 1.5,
+          style: const TextStyle(
+            color: Colors.deepPurple,
+            fontWeight: FontWeight.bold
+          )),
+      ),
+    );
+  }
+}
