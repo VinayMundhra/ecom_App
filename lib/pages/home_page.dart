754bdd8478e5bf836ec9b5a66b9aaa0b7e3defb5
vmundha1199@gmail.com <vmundha1199@gmail.com>
2021-09-16 11:57:04 +0530

    day 6 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,11 @@
 import 'package:catelog/models/catelog.dart';
 import 'package:catelog/widgets/drawer.dart';
 import 'package:catelog/widgets/items_widget.dart';
+import 'package:catelog/widgets/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
+import 'package:velocity_x/velocity_x.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -34,60 +36,115 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Catelog App'),
+      backgroundColor: MyTheme.creamColor,
+      body: SafeArea(
+        child: Container(
+          padding: Vx.m32,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              const CatelogHeader(),
+              if (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+                const CatelogList().expand()
+              else
+                const Center(
+                  child: CircularProgressIndicator(),
+                )
+            ],
+          ),
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 16,
-                    crossAxisSpacing: 16),
-                itemBuilder: (BuildContext context, int index) {
-                  final Item item = CatalogModel.items[index];
-                  return Card(
-                       clipBehavior: Clip.antiAlias,
-                       shape: RoundedRectangleBorder(
-                       borderRadius: BorderRadius.circular(15)),
-                      child: GridTile(
-                        header: Container(
-                          child: Text(
-                            item.name,
-                            style: const TextStyle(color: Colors.white),
-                            ),
-                            decoration: const BoxDecoration(
-                          color: Colors.deepPurple,
-                        ),
-                        padding: const EdgeInsets.all(12),
-                        ),
-
-                        child: 
-                         Image.network(
-                           item.image
-                           ),
-
-                        footer: Container(
-                          child: Text(
-                            item.price.toString(),
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: Colors.black,),
-                            ),
-                            padding: const EdgeInsets.all(12),
-                            decoration: const BoxDecoration(
-                              color: Colors.black54
-                            ),
-                            ),
-                        ));
-                },
-                itemCount: CatalogModel.items.length,
-              )
-            : const Center(
-                child: CircularProgressIndicator(),
-              ),
-      ),
-      drawer: const MyDrawer(),
     );
+  }
+}
+
+class CatelogHeader extends StatelessWidget {
+  const CatelogHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        'Catelog App'.text.xl5.bold.color(MyTheme.darkBluishColor).make(),
+        'Trending products'.text.xl2.make(),
+      ],
+    );
+  }
+}
+
+class CatelogList extends StatelessWidget {
+  const CatelogList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        shrinkWrap: true,
+        itemCount: CatalogModel.items.length,
+        itemBuilder: (BuildContext context, int index) {
+          final Item catalog = CatalogModel.items[index];
+          return CatelogItem(catalog: catalog);
+        });
+  }
+}
+
+class CatelogItem extends StatelessWidget {
+  final Item catalog;
+  // ignore: sort_constructors_first
+  const CatelogItem({Key? key, required this.catalog})
+      : assert(catalog != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+        child: Row(
+      children: [
+        CatalogImage(image: catalog.image),
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            catalog.name.text.lg.color(MyTheme.darkBluishColor).bold.make(),
+            catalog.desc.text.hairLine.sm.make(),
+            10.heightBox,
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceBetween,
+              // buttonPadding: Vx.mOnly(right: 16),
+              buttonPadding: EdgeInsets.zero,
+              children: [
+                "\$${catalog.price}".text.bold.make(),
+                ElevatedButton(
+                  onPressed: (){ 
+                  }, 
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                      MyTheme.darkBluishColor,
+                      ),
+                      shape: MaterialStateProperty.all(StadiumBorder())
+                  ),
+                child: "Buy".text.make())
+              ],
+            ).pOnly(right: 0.0)
+          ],
+        ))
+      ],
+    )).white.roundedLg.square(150).make().py16();
+  }
+}
+
+class CatalogImage extends StatelessWidget {
+  final String image;
+  const CatalogImage({Key? key, required this.image})
+      : assert(image != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      image,
+    ).box.rounded.p8.color(MyTheme.creamColor).make().p16().w40(context);
   }
 }
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+// ignore: avoid_classes_with_only_static_members
 class MyTheme{
   static ThemeData lightTheme (BuildContext context)=> 
     ThemeData(
         primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily, 
+        fontFamily: GoogleFonts.poppins().fontFamily, 
        appBarTheme:  AppBarTheme(
          color: Colors.white,
          elevation: 0.0,
@@ -17,4 +18,8 @@ class MyTheme{
     ThemeData(
        brightness: Brightness.dark 
        );
+
+static Color creamColor = const Color(0xfff5f5f5);
+  static Color darkBluishColor = const Color(0xff403b58);
+
 }
